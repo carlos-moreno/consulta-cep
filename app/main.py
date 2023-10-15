194968f84ad3998c766f409ce074af53851ca9c2
Carlos Moreno <omorenodovale@gmail.com>
2023-10-14 22:31:41 -0300

    (feat): Add route create address

--- app/main.py
+++ app/main.py
@@ -2,10 +2,13 @@ import requests
 from bootstrap import mongodb
 from fastapi import FastAPI, Path, Request
 from fastapi.responses import JSONResponse
+from pymongo import errors
 
 from app import schemas
 
-app = FastAPI()
+app = FastAPI(
+    title="Consulta CEP", summary="Application to consult CEP using Via CEP"
+)
 mongodb.install(app)
 
 
@@ -24,7 +27,9 @@ async def cep_not_found_handler(request: Request, exc: CepNotFoundException):
     )
 
 
-@app.get("/address/{cep}", response_model=schemas.AddressOutput, tags=["search"])
+@app.get(
+    "/address/{cep}", response_model=schemas.AddressOutput, tags=["search"]
+)
 async def search_address_by_cep(cep: str = Path(max_length=9, min_length=9)):
     """Search for an address according to the zip code entered
 
@@ -42,3 +47,20 @@ async def search_address_by_cep(cep: str = Path(max_length=9, min_length=9)):
 
         await save_address(address)
     return address
+
+
+@app.post(
+    "/address",
+    response_model=schemas.AddressOutput,
+    tags=["create"],
+    status_code=201,
+)
+async def create_address(address: schemas.AddressInput):
+    """Create a new address"""
+    try:
+        await save_address(address.dict())
+        return address
+    except errors.DuplicateKeyError:
+        return JSONResponse(
+            status_code=409, content={"message": "CEP informado já existe"}
+        )
